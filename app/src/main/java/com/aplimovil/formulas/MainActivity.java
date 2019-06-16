dd8d7cdf36f24bb1c4ed03ea424239e37c8dd2d1
duvan rojas <duvanjarobena@unicauca.edu.co>
2019-06-16 15:29:07 -0500

    Listview arraylist- class java

--- app/src/main/java/com/aplimovil/formulas/MainActivity.java
+++ app/src/main/java/com/aplimovil/formulas/MainActivity.java
@@ -4,18 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    //Button btn_ecua, btn_salir;
+    Button btn_ecua, btn_salir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //  btn_ecua= (Button) findViewById(R.id.btn_ecua);
-        //btn_salir= (Button) findViewById(R.id.btn_salir);
-        // btn_ecua.setOnClickListener(new View.OnClickListener() { //INTERFACE
-        //  public void onClick(View v) { //CLASE ANINIMA
-        //    Toast.makeText(getApplicationContext(),"SELECCIONÓ ESCUACIONES",Toast.LENGTH_LONG).show();
+        // btn_ecua= (Button) findViewById(R.id.btn_ecua);
+       // btn_salir= (Button) findViewById(R.id.btn_salir);
+         //btn_ecua.setOnClickListener(new View.OnClickListener() { //INTERFACE
+          //public void onClick(View v) { //CLASE ANINIMA
+            //Toast.makeText(getApplicationContext(),"SELECCIONÓ ESCUACIONES", Toast.LENGTH_LONG).show();
         //}
         //});
     }
--- app/src/main/java/com/aplimovil/formulas/subcategoria.java
+++ app/src/main/java/com/aplimovil/formulas/subcategoria.java
@@ -10,6 +10,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class subcategoria extends AppCompatActivity {
  ListView lista_ecua;
@@ -18,8 +19,18 @@ public class subcategoria extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subcategoria);
         lista_ecua= (ListView) findViewById(R.id.list_tipo_ecuaciones);
-        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.arraySub_Category,android.R.layout.simple_list_item_1);
-        lista_ecua.setAdapter(adapter);
+       // ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.arraySub_Category,android.R.layout.simple_list_item_1);
+
+        ArrayList<String> listaEcua=new ArrayList<>();
+        listaEcua.add(" Sistema de Ecuaciones Lineales");
+        listaEcua.add( "Suma de Matrices");
+        listaEcua.add("Suma de Matrices" );
+        listaEcua.add("Sistema de Ecuaciones Lineales ");
+        listaEcua.add( "Suma de Comlejos ");
+        listaEcua.add("Sistema de Ecuaciones Lineales" );
+        listaEcua.add("Producto de Matrices");
+        ArrayAdapter adaptdor =new ArrayAdapter(this,android.R.layout.simple_list_item_1,listaEcua);
+        lista_ecua.setAdapter(adaptdor);
         lista_ecua.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
