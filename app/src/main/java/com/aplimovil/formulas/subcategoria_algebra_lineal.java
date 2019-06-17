31af048299038e84948c81e66f80b9019d9a876f
duvan rojas <duvanjarobena@unicauca.edu.co>
2019-06-16 19:19:05 -0500

    update app

---
+++ app/src/main/java/com/aplimovil/formulas/subcategoria_algebra_lineal.java
@@ -0,0 +1,48 @@
+package com.aplimovil.formulas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class subcategoria_algebra_lineal extends AppCompatActivity {
+ ListView lista_ecua;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subcategoria);
+        lista_ecua= (ListView) findViewById(R.id.list_tipo_ecuaciones);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.arraySub_Category,
+                android.R.layout.simple_list_item_1);
+
+        lista_ecua.setAdapter(adapter);
+        lista_ecua.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(),"seleccionaste: "
+                       +parent.getItemAtPosition(position).toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void onClicc (View view){
+
+        Intent mmintent=null;
+        switch (view.getId() ){
+            case R.id.btn_volver:
+                mmintent= new Intent(subcategoria_algebra_lineal.this,MainActivity.class);
+
+                break;
+            case R.id.bnt_alge:
+                mmintent=new Intent(subcategoria_algebra_lineal.this, MainActivity.class);
+        break;
+        }
+        startActivity(mmintent);
+
+
+        }
+
+        }
